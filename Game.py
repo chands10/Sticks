3f191e3493dab3cf9f05afd72529ca4bed2475aa
Salil Chandra <salilc33@gmail.com>
2021-07-14 18:04:05 -0400

    Added minimax search AI and refactored code

--- Game.py
+++ Game.py
@@ -1,8 +1,16 @@
-from Player import Player
+from Player import *
 
-class Game():
-    def __init__(self):
+class Game:
+    def __init__(self, numHumans):
         self.players = [Player(), Player()]
+        self.ais = [False, False]
+        if numHumans <= 1:
+            self.players[1] = AI(1)
+            self.ais[1] = True
+        if numHumans == 0:
+            self.players[0] = AI(0)
+            self.ais[0] = True
+        
         self.turn = 0
         self.other = 1
         
@@ -11,30 +19,23 @@ class Game():
         for i, player in enumerate(self.players):
             print("Player {}: {}".format(i + 1, player))
             
-        move = input("Player {} enter move: ".format(self.turn + 1))
-        move = move.split()
-        
-        if len(move) != 3:
-            raise Exception("Invalid move")
-        
-        move[1] = int(move[1])
-        move[2] = int(move[2])
-        
-        if move[0] == "swap":
-            self.players[self.turn].swap(move[1:])
-        elif move[0] == "tap":
-            finished = self.players[self.turn].tap(self.players[self.other], move[1], move[2])
-            if finished:
-                print("Player {} wins!".format(self.turn + 1))
-                return True
+        if self.ais[self.turn]:
+            move = self.players[self.turn].getOptimalMove(self.players[self.other])
+            print(move)
         else:
-            raise Exception("Invalid move")
+            move = input("Player {} enter move: ".format(self.turn + 1))
+        
+        r = generateSuccessorState(self.players, self.turn, self.other, move)
+        if r:
+            print("Player {} wins!".format(self.turn + 1))
         
         self.turn, self.other = self.other, self.turn
-        return False
-    
+        
+        return r
+        
 if __name__ == "__main__":
-    game = Game()
+    numHumans = int(input("Input number of humans (0 - 2): "))
+    game = Game(numHumans)
     while True:
         try:
             finished = game.makeMove()
--- Player.py
+++ Player.py
@@ -1,9 +1,15 @@
+from copy import deepcopy
+from math import inf
+
 class Player:
     def __init__(self):
         self.hands = [1, 1]
         
     def __str__(self):
         return str(self.hands)
+    
+    def evaluation(self, player2):
+        return sum(player2.hands) - sum(self.hands)
     
     def getPossibleActions(self, player2):
         actions = []
@@ -57,3 +63,78 @@ class Player:
             
         return sum(self.hands) == 0
     
+class AI(Player):
+    def __init__(self, idx):
+        super().__init__()
+        self.idx = idx
+        self.other = (idx + 1) % 2
+        self.depth = 3
+    
+    def getOptimalMove(self, player2):
+        players = [player2, player2]
+        players[self.idx] = self
+        
+        def maxValue(players, depth):
+            best = (-inf, "")
+            
+            moves = players[self.idx].getPossibleActions(players[self.other])
+            for move in moves:
+                newPlayers = deepcopy(players)
+                if generateSuccessorState(newPlayers, self.idx, self.other, move): # AI wins at this point, no need to travel deeper
+                    return (inf, move)
+                best = max(best, value(newPlayers, depth + 0.5, move))
+                
+            if len(moves) == 0:
+                best = (players[self.idx].evaluation(players[self.other]), None)
+            
+            return best
+        
+        def minValue(players, depth):
+            best = inf
+            
+            moves = players[self.other].getPossibleActions(players[self.idx])
+            for move in moves:
+                newPlayers = deepcopy(players)
+                if generateSuccessorState(newPlayers, self.other, self.idx, move): # opponent wins at this point, no need to travel deeper
+                    return -inf
+                
+                best = min(best, value(newPlayers, depth + 0.5)[0])
+                
+            if len(moves) == 0:
+                best = (players[self.idx].evaluation(players[self.other]), None)
+            
+            return best
+        
+        def value(players, depth, move=None):
+            if depth == self.depth:
+                return (players[self.idx].evaluation(players[self.other]), move)
+            findMax = depth % 1 == 0
+            if findMax:
+                return maxValue(players, depth)
+            else:
+                return (minValue(players, depth), move)
+            
+        
+        move = value(players, 0)
+        return move[1]
+    
+    
+def generateSuccessorState(players, turn, other, move):
+    move = move.split()
+    
+    if len(move) != 3:
+        raise Exception("Invalid move")
+    
+    move[1] = int(move[1])
+    move[2] = int(move[2])
+    
+    if move[0] == "swap":
+        players[turn].swap(move[1:])
+    elif move[0] == "tap":
+        finished = players[turn].tap(players[other], move[1], move[2])
+        if finished:
+            return True
+    else:
+        raise Exception("Invalid move")
+    
+    return False
